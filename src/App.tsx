11d7002b17a0e9836dbc9c608423cc64da8e76f9
MahmudulHasan1316 <mahmudhj1316@gmail.com>
2026-09-13 02:12:50 +0600

    This is my first commit for this project & I have installed some necessary tool.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-amber-300 text-6xl">My Development Stack</div>
+      <button className="btn btn-primary">Primary</button>
+    </>
+  )
+}
+
+export default App
